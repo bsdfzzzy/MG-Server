586fe70665eb631c9fc84f6c8976ccace4320fbe
bsdfzzzy <1002380934@qq.com>
2016-07-28 16:53:10 +0800

    add change password api

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -5,6 +5,27 @@ const findAll = require('../utils/findAll');
 const addOne = require('../utils/addOne');
 
 router.get('/getAll', findAll(User));
+
 router.post('/addOne', addOne(User));
+
+router.put('/changePass', async (ctx, next) => {
+    let data = ctx.request.body;
+    let newPass = {
+        password: data.password
+    };
+    try {
+        let newOne = await User.update(newPass, {
+            where: {
+                account: {
+                    in: [data.account]
+                } 
+            }
+        });
+        ctx.body = {code: 1, message: "success update", update: newOne};
+    } catch (err) {
+        console.log(err);
+        ctx.throw(err);
+    }
+});
 
 module.exports = router;
